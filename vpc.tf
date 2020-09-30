00692e63c55ad343bdf6b5716ec54df2da887eef
Vadim Girenko <vadim.girenko@idealcorp.com>
2020-09-30 22:24:33 +0300

    outputs commented

--- vpc.tf
+++ vpc.tf
@@ -1,75 +1,11 @@
-resource aws_vpc my_vpc {
-  cidr_block = var.vpc_cidr_block
-  tags = {
-    Name = "my_vpc"
-  }
-}
-resource aws_internet_gateway igw {
-  vpc_id = aws_vpc.my_vpc.id
-}
-data aws_availability_zones my_zones {
-  state = "available"
-}
-resource aws_subnet public-subnets {
-  count                   = length(var.public_subnets)
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = element(var.public_subnets, count.index)
-  map_public_ip_on_launch = true
-  availability_zone       = data.aws_availability_zones.my_zones.names[count.index]
-  depends_on              = [aws_internet_gateway.igw]
-  tags = {
-    Name = "public_subnet_${count.index+1}"
-  }
-}
-resource aws_subnet private-subnets {
-  count                   = length(var.private_subnets)
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = element(var.private_subnets, count.index)
-  map_public_ip_on_launch = false
-  availability_zone       = data.aws_availability_zones.my_zones.names[count.index]
-  tags = {
-    Name = "private_subnet_${count.index+1}"
-  }
-}
-resource aws_eip nat {
-  count = length(var.public_subnets)
-  vpc = true
-}
-resource aws_nat_gateway nat {
-  depends_on    = [aws_internet_gateway.igw]
-  count         = length(var.public_subnets)
-  allocation_id = aws_eip.nat[count.index].id
-  subnet_id     = aws_subnet.public-subnets[count.index].id
-}
-resource aws_route_table public {
-  vpc_id = aws_vpc.my_vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-}
-resource aws_route_table_association public-subnets {
-  count          = length(var.public_subnets)
-  subnet_id      = element(aws_subnet.public-subnets.*.id, count.index)
-  route_table_id = aws_route_table.public.id
-}
-resource aws_security_group my_group {
-  name = "my_security_group"
+module vpc {
+  source = "terraform-aws-modules/vpc/aws"
 
-  dynamic "ingress" {
-    for_each = var.ingress_ports
-    content {
-      from_port = ingress.value
-      to_port   = ingress.value
-      protocol  = "tcp"
-    }
-  }
-  dynamic "egress" {
-    for_each = var.egress_ports
-    content {
-      from_port = egress.value
-      to_port   = egress.value
-      protocol  = "tcp"
-    }
-  }
+  name               = "vpc-module"
+  cidr               = "10.0.0.0/16"
+  azs                = ["eu-west-1a", "eu-west-1b"]
+  private_subnets    = ["10.0.1.0/24", "10.0.3.0/24"]
+  public_subnets     = ["10.0.2.0/24", "10.0.4.0/24"]
+  enable_nat_gateway = true
+
 }
