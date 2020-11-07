10ac523170acf7ffc8935efc21fc0d8e0fd64eea
Vadim Girenko <vadim.girenko@idealcorp.com>
2020-11-07 22:49:04 +0200

    messy fixed

--- eks.tf
+++ eks.tf
@@ -1,26 +1,26 @@
-data "aws_eks_cluster" "cluster" {
-  name = module.vg-cluster.cluster_id
-}
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.vg-cluster.cluster_id
-}
-data "aws_subnet" "private" {
-  for_each = aws_subnet.private-subnets.id
-
-  id = each.value
-}
-module "vg-cluster" {
-  source          = "terraform-aws-modules/eks/aws"
-  #count           = 1
-  cluster_name    = "vg-cluster"
-  cluster_version = "1.17"
-  subnets         = data.aws_subnet.private.id
-  vpc_id          = aws_vpc.vg_vpc.id
-
-  worker_groups = [
-    {
-      instance_type = "a1.large"
-      asg_max_size  = 5
-    }
-  ]
-}
+//data "aws_eks_cluster" "cluster" {
+//  name = module.vg-cluster.cluster_id
+//}
+//data "aws_eks_cluster_auth" "cluster" {
+//  name = module.vg-cluster.cluster_id
+//}
+//data "aws_subnet" "private" {
+//  for_each = aws_subnet.private-subnets.id
+//
+//  id = each.value
+//}
+//module "vg-cluster" {
+//  source          = "terraform-aws-modules/eks/aws"
+//  #count           = 1
+//  cluster_name    = "vg-cluster"
+//  cluster_version = "1.17"
+//  subnets         = data.aws_subnet.private.id
+//  vpc_id          = aws_vpc.vg_vpc.id
+//
+//  worker_groups = [
+//    {
+//      instance_type = "a1.large"
+//      asg_max_size  = 5
+//    }
+//  ]
+//}
--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   region = var.aws_region
 }
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
-  version                = "~> 1.9"
-}
+//provider "kubernetes" {
+//  host                   = data.aws_eks_cluster.cluster.endpoint
+//  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+//  token                  = data.aws_eks_cluster_auth.cluster.token
+//  load_config_file       = false
+//  version                = "~> 1.9"
+//}
