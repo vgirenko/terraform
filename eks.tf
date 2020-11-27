42f3ba9c97aeae142a17769b98962762419781a5
Vadim Girenko <vadim.girenko@idealcorp.com>
2020-11-27 17:33:38 +0200

    spots added

---
+++ eks.tf
@@ -0,0 +1,35 @@
+data "aws_eks_cluster" "cluster" {
+  name = module.vg-cluster.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.vg-cluster.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.9"
+}
+
+module "vg-cluster" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "vg-cluster"
+  cluster_version = "1.18"
+  subnets         = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  worker_groups_launch_template = [
+    {
+      name                    = "spot-1"
+      override_instance_types = ["m5.large", "m5a.large", "m5d.large", "m5ad.large"]
+      spot_instance_pools     = 3
+      asg_max_size            = 3
+      asg_desired_capacity    = 2
+      kubelet_extra_args      = "--node-labels=node.kubernetes.io/lifecycle=spot"
+      public_ip               = true
+    },
+  ]
+}
--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,4 @@
-data aws_availability_zones my_zones {
+data aws_availability_zones vg_zones {
 state = "available"
 }
 
@@ -6,7 +6,7 @@ module vpc {
   source = "terraform-aws-modules/vpc/aws"
   name               = "vpc-module"
   cidr               = "10.0.0.0/16"
-  azs                = data.aws_availability_zones.my_zones.names[count.index]
+  azs                = data.aws_availability_zones.vg_zones.names
   private_subnets    = ["10.0.1.0/24", "10.0.3.0/24"]
   public_subnets     = ["10.0.2.0/24", "10.0.4.0/24"]
   enable_nat_gateway = true
