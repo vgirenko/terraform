409e05c5e14a9a809b2e45ab411fe270b4090fc7
Vadym Gyrenko <girenko.vadim@gmil.com>
2021-01-30 18:11:24 +0200

    node group added

--- eks.tf
+++ eks.tf
@@ -11,3 +11,16 @@ resource aws_eks_cluster vg_eks_cluster {
     aws_iam_role_policy_attachment.eks_cluster_eks_vpc_resource_controller,
   ]
 }
+resource aws_eks_node_group vg-node-group {
+  cluster_name = aws_eks_cluster.vg_eks_cluster.name
+  node_group_name = "vg-eks-node-group"
+  node_role_arn = aws_iam_role.vg_eks_node_group.name
+  subnet_ids = [aws_subnet.private-subnets[0].id, aws_subnet.private-subnets[1].id]
+  capacity_type = SPOT
+
+  scaling_config {
+    desired_size = 1
+    max_size = 1
+    min_size = 1
+  }
+}
--- iam.tf
+++ iam.tf
@@ -20,3 +20,19 @@ resource aws_iam_role_policy_attachment eks_cluster_eks_vpc_resource_controller 
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   role       = aws_iam_role.vg_eks_cluster_role.name
 }
+resource aws_iam_role vg_eks_node_group {
+  name = "vg-eks-mode-group"
+  assume_role_policy = data.aws_iam_policy_document.eks_assume_role_policy.json
+}
+resource aws_iam_policy_attachment AmazonEKSWorkerNodePolicy {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.vg_eks_node_group.name
+}
+resource aws_iam_policy_attachment AmazonEKS_CNI_Policy {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.vg_eks_node_group.name
+}
+resource aws_iam_role_policy_attachment AmazonEC2ContainerRegistryReadOnly {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.vg_eks_node_group.name
+}
